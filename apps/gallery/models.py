56d05e1534356e67e5fd36ff848a0b11c27ec90d
mr.propya <maheshtamse.13@gmail.com>
2019-08-28 14:32:28 +0530

    apps

--- apps/gallery/models.py
+++ apps/gallery/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class GalleryImage(models.Model):
+    displayText = models.CharField(max_length=100)
+    imageUrl = models.CharField(max_length=500)
+
+
+class GalleryVideo(models.Model):
+    displayText = models.CharField(max_length=100)
+    videoUrl = models.CharField(max_length=500)
