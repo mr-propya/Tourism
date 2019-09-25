89e15116dd4ee923b4dea9b7274218ee7e0a03ab
muskan996091 <42430178+muskan996091@users.noreply.github.com>
2019-09-25 14:30:55 +0530

    jnjjnn

--- Tourism/urls.py
+++ Tourism/urls.py
@@ -14,7 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,5 +27,9 @@ urlpatterns = [
     path('home/', include('apps.home.urls')),
     path('events/', include('apps.events.urls')),
     path('experience/', include('apps.experience.urls')),
-    path('festival/', include('apps.festival.urls'))
+    path('festival/', include('apps.festival.urls')),
+    re_path(r'about-us.html/', include('apps.aboutus.urls')),
+    re_path(r'contact-us.html/', include('apps.contact.urls')),
+    re_path(r'faq.html/', include('apps.faq.urls')),
+    re_path(r'feedback.html/', include('apps.feedback.urls')),
 ]
